da854a46e1c17c65b60387761fc819c25ead383d
LuckyC0der99 <49605145+LuckyC0der99@users.noreply.github.com>
2019-12-11 00:05:10 +0800

    矩阵特征值与条件数计算。

--- 矩阵特征值与条件数计算.c
+++ 矩阵特征值与条件数计算.c
@@ -113,13 +113,13 @@ int main()
 	for(i=0;i<max1;i++){
 		t_k[i]=0.05*i;
 	}
-	
+
 	for(t=0;t<max1;t++){
 	initArray(A,t_k[t]);
 	initArray(Ani,t_k[t]);
 	}
 	for ( num_x=0; num_x < max1; num_x++) {
-		
+
 		maxcond = 0, mincond = 10000;
 		initArray(A,t_k[num_x]);
 		for(i=0;i<max0;i++){
@@ -129,27 +129,27 @@ int main()
 		e = 0.0000001, delta = 1;
 		iter = 0;//定义迭代次数 
 		do
-        {        
-                ++iter;                
-            maxElemt1=GetMax(X);
- 
-                for (int j=0 ;j<max0;++j)
-                {
-                        *(X+j)=*(X+j)/maxElemt1;                //列向量归一化
-                }
-                matrixMulti(A,X);                
-                maxElemt2 = GetMax(X);
-                minElemt2 = GetMin(X);
-            
-        }while(fabs((maxElemt2-maxElemt1)/maxElemt1 )> 0.0000001);
-        conddd[num_x]=maxElemt2/minElemt2;
-    printf("t_k = %e,",t_k[num_x]);
-    printf("迭代次数 = %d,",iter);
-    printf("最大特征值 = %d\n",maxElemt2);
-       // cout<<" t_k = "<<t_k[num_x];
+	{        
+		++iter;                
+	    maxElemt1=GetMax(X);
+
+		for (int j=0 ;j<max0;++j)
+		{
+			*(X+j)=*(X+j)/maxElemt1;                //列向量归一化
+		}
+		matrixMulti(A,X);                
+		maxElemt2 = GetMax(X);
+		minElemt2 = GetMin(X);
+
+	}while(fabs((maxElemt2-maxElemt1)/maxElemt1 )> 0.0000001);
+	conddd[num_x]=maxElemt2/minElemt2;
+	printf("t_k = %e,",t_k[num_x]);
+	printf("迭代次数 = %d,",iter);
+	printf("最大特征值 = %d\n",maxElemt2);
+	// cout<<" t_k = "<<t_k[num_x];
 		//cout<<" 迭代次数n = "<<iter;
-        //cout<<" 最大特征值="<<maxElemt2<<endl;
-        //cout<<"最小特征数="<<minElemt2<<endl;
+	//cout<<" 最大特征值="<<maxElemt2<<endl;
+	//cout<<"最小特征数="<<minElemt2<<endl;
 	}
 	maxcond = GetMax(conddd); 
 	mincond = GetMin(conddd);
